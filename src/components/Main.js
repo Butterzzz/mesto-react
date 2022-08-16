f3aea6531ee641bca64c761aa2132f51100cec60
Grinchenko Sergei <butters.blaga@gmail.com>
2022-08-17 03:21:53 +0900

    feat: add imperative handlers (popups work)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,15 @@
+function handleEditAvatarClick() {
+    document.querySelector('.popup_type_avatar').classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+    document.querySelector('.popup_type_edit-profile').classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+    document.querySelector('.popup_type_add-card').classList.add('popup_opened');
+}
+
 function Main() {
     return (
         <main className="content">
@@ -5,17 +17,17 @@ function Main() {
             <section className="profile">
 
                 <img className="profile__image" src="<%=require('./images/avatar.jpg')%>" alt="Изображение профиля" />
-                <button type="button" className="profile__button profile__button_action_edit-avatar"></button>
+                <button className="profile__button profile__button_action_edit-avatar" type="button" aria-label="Редактировать аватар" onClick={handleEditAvatarClick}></button>
 
                 <div className="profile__info">
                     <h1 className="profile__name">Жак-Ив Кусто</h1>
                     <button className="profile__button profile__button_action_edit" type="button"
-                        aria-label="Редактировать профиль"></button>
+                        aria-label="Редактировать профиль" onClick={handleEditProfileClick}></button>
                     <p className="profile__about">Исследователь океана</p>
                 </div>
 
                 <button className="profile__button profile__button_action_add" type="button"
-                    aria-label="Добавить фотографию"></button>
+                    aria-label="Добавить фотографию" onClick={handleAddPlaceClick}></button>
 
             </section>
 
@@ -28,4 +40,5 @@ function Main() {
         </main>
     );
 }
+
 export default Main;
