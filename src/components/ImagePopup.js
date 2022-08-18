6436885b428fca5f7aa81eb3ef0afa318bb494ec
Grinchenko Sergei <butters.blaga@gmail.com>
2022-08-19 05:13:36 +0900

    style: correct code style

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,15 +1,15 @@
 function ImagePopup(props) {
-    return (
-        <div className={`popup popup_type_${props.name} ${props.card && "popup_opened"}`}>
-        <div className="popup__image-container">
-          <figure className="popup__figure">
-            <img className="popup__image" src={props.card && props.card.link} alt={props.card && props.card.name} />
-            <figcaption className="popup__image-caption">{props.card && props.card.name}</figcaption>
-          </figure>
-          <button className="popup__button popup__button_action_close" type="button" aria-label="Закрыть окно просмотра" onClick={props.onClose}></button>
-        </div>
+  return (
+    <div className={`popup popup_type_${props.name} ${props.card && "popup_opened"}`}>
+      <div className="popup__image-container">
+        <figure className="popup__figure">
+          <img className="popup__image" src={props.card && props.card.link} alt={props.card && props.card.name} />
+          <figcaption className="popup__image-caption">{props.card && props.card.name}</figcaption>
+        </figure>
+        <button className="popup__button popup__button_action_close" type="button" aria-label="Закрыть окно просмотра" onClick={props.onClose}></button>
       </div>
-    );
+    </div>
+  );
 }
 
 export default ImagePopup;
