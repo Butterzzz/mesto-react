acad193b6e7b81f9ee04de4f56afbfb5e0480ad1
Grinchenko Sergei <butters.blaga@gmail.com>
2022-09-01 02:41:21 +0900

    refactor: add context in EditProfilePopup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 import PopupWithForm from './PopupWithForm';
 
 function EditProfilePopup({ isOpen, onClose }) {
+    const currentUser = React.useContext(CurrentUserContext);
+
+    useEffect(() => {
+        setName(currentUser.name);
+        setDescription(currentUser.about);
+    }, [currentUser]);
 
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
@@ -24,10 +31,10 @@ function EditProfilePopup({ isOpen, onClose }) {
             buttonText={'Сохранить'}>
             <fieldset className="popup__input-container">
                 <input className="popup__input" type="text" name="name" id="name-input" minLength="2" maxLength="40"
-                    placeholder="Имя" required value={name} onChange={handleChangeName}/>
+                    placeholder="Имя" required value={name} onChange={handleChangeName} />
                 <span className="popup__error name-input-error"></span>
                 <input className="popup__input" type="text" name="about" id="about-input" minLength="2" maxLength="200"
-                    placeholder="О себе" required value={description} onChange={handleDescription}/>
+                    placeholder="О себе" required value={description} onChange={handleDescription} />
                 <span className="popup__error about-input-error"></span>
             </fieldset>
         </PopupWithForm>
