7425fffe59cc3d6d93881baa3e8398e4979b5495
Grinchenko Sergei <butters.blaga@gmail.com>
2022-09-01 02:35:37 +0900

    refactor: add controlled components in EditProfilePopup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PopupWithForm from './PopupWithForm';
 
-function EditProfilePopup({  isOpen, onClose }) {
+function EditProfilePopup({ isOpen, onClose }) {
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+    }
+
+    function handleDescription(e) {
+        setDescription(e.target.value);
+    }
+
     return (
         <PopupWithForm
-          isOpen={isOpen}
-          onClose={onClose}
-          name={'edit-profile'}
-          form={'editProfile'}
-          title={'Редактировать профиль'}
-          buttonText={'Сохранить'}>
-          <fieldset className="popup__input-container">
-            <input className="popup__input" type="text" name="name" id="name-input" minLength="2" maxLength="40"
-              placeholder="Имя" required />
-            <span className="popup__error name-input-error"></span>
-            <input className="popup__input" type="text" name="about" id="about-input" minLength="2" maxLength="200"
-              placeholder="О себе" required />
-            <span className="popup__error about-input-error"></span>
-          </fieldset>
+            isOpen={isOpen}
+            onClose={onClose}
+            name={'edit-profile'}
+            form={'editProfile'}
+            title={'Редактировать профиль'}
+            buttonText={'Сохранить'}>
+            <fieldset className="popup__input-container">
+                <input className="popup__input" type="text" name="name" id="name-input" minLength="2" maxLength="40"
+                    placeholder="Имя" required value={name} onChange={handleChangeName}/>
+                <span className="popup__error name-input-error"></span>
+                <input className="popup__input" type="text" name="about" id="about-input" minLength="2" maxLength="200"
+                    placeholder="О себе" required value={description} onChange={handleDescription}/>
+                <span className="popup__error about-input-error"></span>
+            </fieldset>
         </PopupWithForm>
     );
 }
